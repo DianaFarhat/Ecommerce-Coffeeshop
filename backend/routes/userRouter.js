63366f0a5abde347875cc82af5e8c007c3ea687a
Marwa Bikai <marwabikai@hotmail.com>
2025-01-30 23:30:00 +0200

    fixed router/login line ,to start with login part

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -4,7 +4,7 @@ const {signup,login} = require('../controllers/userController');
 
 
   router.post('/signup', signup); // This maps POST /signup to the signup function
-  router.post('/auth', login); // This maps POST /signup to the signup function
+  router.post('/auth', login); // This maps POST /login to the login function
 
  
   module.exports = router; 
