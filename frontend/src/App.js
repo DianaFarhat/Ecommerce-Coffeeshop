cc89cdcd63df14216a062f9c2496a09ec0a35f55
Marwa Bikai <marwabikai@hotmail.com>
2025-01-31 14:12:21 +0200

    added some components for testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Signup from './components/signup'
+import Login from './components/login'
+// import Navigation from './pages/Auth/Navigation.jsx'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 
 function App() {
   return (
+    <>
+   
    <Signup></Signup>
+   <Login/></>
   );
 }
 
