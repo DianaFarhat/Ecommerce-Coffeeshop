4a3b48ae89a7b85238f9b59606aafdb7920bcbde
DianaFarhat <dsf04@mail.aub.edu>
2025-02-03 13:43:57 +0200

    Edit Carousel.jsx,  Product model, and Order Model

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -1,64 +1,90 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose= require("mongoose");
+const Schema= mongoose.Schema;
 
-const orderSchema = new Schema({
-    orderOwner: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
+const orderSchema = new mongoose.Schema(
+  {
+    user: { type: Schema.Types.ObjectId, required: true, ref: "User" },
     orderItems: [
       {
-        productId: {
+        name: { type: String, required: true },
+        qty: { type: Number, required: true },
+        image: { type: String, required: true },
+        price: { type: Number, required: true },
+        product: {
           type: Schema.Types.ObjectId,
+          required: true,
           ref: "Product",
-          required: true,
-        },
-        quantity: {
-          type: Number,
-          required: true,
-          default: 1,
-        },
-        appliedDiscount: {
-          type: Boolean,
-          default: false, // Tracks if a discount was applied (only if product is part of a bundle and all bundle items are present in the cart)
-        },
-        price: {
-          type: Number, // Price of the product at the time of purchase
-          required: true,
         },
       },
     ],
-    orderStatus: {
-      type: String,
-      default: "pending",
-      enum: ["pending", "cancelled", "completed"],
+
+    shippingAddress: {
+      address: { type: String, required: true },
+      city: { type: String, required: true },
+      postalCode: { type: String, required: true },
+      country: { type: String, required: true },
     },
-    totalPrice: {
-      type: Number,
-      required: true, // You could dynamically calculate this based on orderItems
-      min: 0,
-    },
-    paymentStatus: {
-      type: String,
-      enum: ["pending", "completed", "failed"],
-      default: "pending",
-    },
+
     paymentMethod: {
       type: String,
-      enum: ["cash", "visa", "other"],
       required: true,
     },
-    shippingAddress: {
-      type: String,
-      required: true,
+
+    paymentResult: {
+      id: { type: String },
+      status: { type: String },
+      update_time: { type: String },
+      email_address: { type: String },
     },
-    billingAddress: {
-      type: String,
+
+    itemsPrice: {
+      type: Number,
       required: true,
+      default: 0.0,
+    },
+
+    taxPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+
+    shippingPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+
+    totalPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+
+    isPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+
+    paidAt: {
+      type: Date,
+    },
+
+    isDelivered: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+
+    deliveredAt: {
+      type: Date,
     },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  }
 );
 
+// Export the Product model
 module.exports = mongoose.model("Order", orderSchema);
--- frontend/src/pages/Products/ProductCarousel.jsx
+++ frontend/src/pages/Products/ProductCarousel.jsx
@@ -14,9 +14,33 @@ import {
 } from 'react-icons/fa'
 
 const ProductCarousel = () => {
+  const {data: products, isLoading, error}= useGetTopProductsQuery()  
+
+  const settings= {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+
+  }
+
   return (
-    <div>
-      Product Carousel
+    <div className='mb-4 xl:block lg:block md:block'>
+      {isLoading ? null: error? (<Message variant='danger'>{error?.data?.message || error.message}</Message>): 
+      <Slider {...settings} className='xl:w-[50rem] lg:w-[50rem] md:-[56rem] sm:w-[40rem] sm:block'>
+        {products.map(({image, _id, name, price, description, brand, createdAt, numReviews, rating, quantity, countInStock})=>{
+            <div key= {_id}>
+                <img src={image} alt= {name} className='w-full rounded-lg object-cover h-[30rem]'/>
+            </div>
+        }
+    
+        )}
+      </Slider>
+      }
     </div>
   )
 }
