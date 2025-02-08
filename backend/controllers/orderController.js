8925500d79661b00ec19cff71287d2cf834e9a7d
Marwa Bikai <marwabikai@hotmail.com>
2025-02-08 19:02:21 +0200

    fixed order bug

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,6 +1,6 @@
 const Order =require ("../models/orderModel.js");
 const Product = require ("../models/productModel.js");
-
+const mongoose =require("mongoose")
 // Utility Function
 function calcPrices(orderItems) {
   const itemsPrice = orderItems.reduce(
@@ -74,7 +74,7 @@ exports.createOrder = async (req, res) => {
 
     const order = new Order({
       orderItems: dbOrderItems,
-      user: mongoose.Types.ObjectId(user), // ✅ Convert user to ObjectId
+      user: new mongoose.Types.ObjectId(user), // ✅ Convert user to ObjectId
       shippingAddress,
       paymentMethod,
       itemsPrice,
--- backend/routes/orderRouter.js
+++ backend/routes/orderRouter.js
@@ -1,12 +1,33 @@
-const express= require('express')
-const router=express.Router();
-const userController= require("../controllers/userController");
-const orderController= require("../controllers/orderController");
+const express =  require("express");
+const router = express.Router();
 
+const  {
+  createOrder,
+  getAllOrders,
+  getUserOrders,
+  countTotalOrders,
+  calculateTotalSales,
+  calcualteTotalSalesByDate,
+  findOrderById,
+  markOrderAsPaid,
+  markOrderAsDelivered,
+} =require("../controllers/orderController.js");
 
-router.post("/new-order", userController.protect, orderControllerController.createNewOrder);
+const { authenticate } =require( "../middlewares/authMiddleware.js");
 
+router
+  .route("/")
+  .post(authenticate, createOrder)
+  //.get(authenticate, getAllOrders);
 
+router.route("/mine").get(authenticate, getUserOrders);
+router.route("/total-orders").get(countTotalOrders);
+router.route("/total-sales").get(calculateTotalSales);
+router.route("/total-sales-by-date").get(calcualteTotalSalesByDate);
+router.route("/:id").get(authenticate, findOrderById);
+router.route("/:id/pay").put(authenticate, markOrderAsPaid);
+router
+  .route("/:id/deliver")
+  .put(authenticate, markOrderAsDelivered);
 
-
-module.exports = router; 
+  module.exports = router; 
